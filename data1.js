c1de89bc702c190aa0d6a2f9c92ee83235c0195a
AbelCraftOk <pedrogabrielojeda664@gmail.com>
2025-02-07 02:20:59 -0300

    Add files via upload

---
+++ data1.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = `
+    <p>vegetta777</p>
+    <p>argvegetta7@gmail.com</p>
+`;
---
+++ data2.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = `
+    <p>777vegetta777</p>
+    <p>argvegetta777</p>
+`;
